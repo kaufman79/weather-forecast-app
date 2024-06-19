97d9590cce7e4922c8afed7f0d631782974c8084
kaufman79 <kaufman79@gmail.com>
2024-06-19 13:00:37 -0400

    adds backend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
@@ -10,7 +10,8 @@ option = st.selectbox("Select the data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-temperatures = [10, 11, 15]
+
+data = get_data(place, days, option)
+
 figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(C)"})
 st.plotly_chart(figure)
